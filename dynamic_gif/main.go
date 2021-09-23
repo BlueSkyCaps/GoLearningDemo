994432f54fc5442e6915dc60c2a3d45a35514980
BlueSkyCaps <linsongxian@outlook.com>
2021-09-23 21:36:42 +0800

    update gif examples.

--- dynamic_gif/main.go
+++ dynamic_gif/main.go
@@ -8,26 +8,58 @@ import (
 	"log"
 	"os"
 	"path"
+	"regexp"
+	"runtime"
 	"runtime/debug"
 )
 
+const separator = string(os.PathSeparator)
+
 var (
-	imagesRoot  = "C:/Users/pc/Desktop/gifs/gif"
-	gifOutRoot  = "C:/Users/pc/Desktop/"
-	imgNmeFiles []string
+	imagesInputRoot string
+	gifOutRoot, _   = os.UserHomeDir()
+	imgNmeFiles     []string
+
+	// MatchImageFormat 匹配以常见图像文件格式为后缀的正则表达模式
+	MatchImageFormat = fmt.Sprintf("%v.*(%v.png|%v.gif|%v.jpg|%v.jpeg)$", separator, separator, separator, separator, separator)
 )
 
+func init() {
+	if runtime.GOOS == "windows" {
+		gifOutRoot = path.Join(gifOutRoot, "desktop")
+	}
+}
 func main() {
+	fmt.Println("请输入你想要用于制作动图Gif的图片所在的文件夹路径。(直接粘贴路径并回车即可)")
+	_, e := fmt.Scanf("%s", &imagesInputRoot)
+	if e != nil {
+		fmt.Println(e.Error())
+		return
+	}
+	fmt.Println(imagesInputRoot)
+	fi, e := os.Stat(imagesInputRoot)
+	if e != nil || !fi.IsDir() {
+		println("不存在输入的这个文件夹哦(⊙o⊙)？检查：你输入的路径是文件夹吗，文件夹存在吗？")
+		return
+	}
+	dirInfo, _ := os.ReadDir(imagesInputRoot)
 
-	dirInfo, _ := os.ReadDir(imagesRoot)
 	for i := 0; i < len(dirInfo); i++ {
 		if !dirInfo[i].IsDir() {
-			imgNmeFiles = append(imgNmeFiles, dirInfo[i].Name())
+			if MatchRegexString(MatchImageFormat, dirInfo[i].Name()) {
+				imgNmeFiles = append(imgNmeFiles, dirInfo[i].Name())
+			}
 		}
 	}
+
+	if len(imgNmeFiles) <= 0 {
+		println("文件夹中没有一张图片？检查：文件夹有至少一张图片格式的文件吗？制作动态gif,你应该不止一张源图片。")
+		return
+	}
+
 	inputGifBoss := &gif.GIF{}
-	for i, currentGifName := range imgNmeFiles {
-		g, _ := os.Open(path.Join(imagesRoot, currentGifName))
+	for _, currentGifName := range imgNmeFiles {
+		g, _ := os.Open(path.Join(imagesInputRoot, currentGifName))
 		currentGifImage, err := gif.Decode(g)
 		if err != nil {
 			println(err.Error())
@@ -37,9 +69,9 @@ func main() {
 		_ = g.Close()
 		fmt.Printf("%v\n", currentGifImage.Bounds())
 		inputGifBoss.Image = append(inputGifBoss.Image, currentGifImage.(*image.Paletted))
-		inputGifBoss.Delay = append(inputGifBoss.Delay, (i+1)*100)
+		inputGifBoss.Delay = append(inputGifBoss.Delay, 100)
 	}
-	inputGifBoss.Config = image.Config{ColorModel: inputGifBoss.Config.ColorModel, Width: 500, Height: 500}
+	inputGifBoss.Config = image.Config{ColorModel: inputGifBoss.Config.ColorModel, Width: 200, Height: 200}
 	inputGifBoss.LoopCount = -1
 	finalGif, _ := os.OpenFile(path.Join(gifOutRoot, UuidGenerator()+"out.gif"), os.O_CREATE|os.O_WRONLY|os.O_TRUNC, os.ModePerm)
 
@@ -57,6 +89,18 @@ func main() {
 	}
 }
 
+func MatchRegexString(p, v string) bool {
+	var e error
+	var m bool
+	if m, e = regexp.MatchString(p, v); m && e == nil {
+		return true
+	}
+	if e != nil {
+		println("error in MatchRegexString", e.Error())
+	}
+	return false
+}
+
 func UuidGenerator() string {
 	b := make([]byte, 16)
 	_, err := rand.Read(b)
@@ -66,36 +110,4 @@ func UuidGenerator() string {
 	}
 	return fmt.Sprintf("%x-%x-%x-%x-%x",
 		b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
-}
-
-func unityRecoverSrcImages() {
-	for i := 0; i < len(imgNmeFiles); i++ {
-		inputGifBossTmp := &gif.GIF{}
-		f, _ := os.Open(path.Join(imagesRoot, imgNmeFiles[i]))
-		currentGifImage, err := gif.Decode(f)
-		if err != nil {
-			println(err.Error())
-			debug.PrintStack()
-			return
-		}
-		_ = f.Close()
-
-		inputGifBossTmp.Image = append(inputGifBossTmp.Image, currentGifImage.(*image.Paletted))
-		inputGifBossTmp.Delay = append(inputGifBossTmp.Delay, 0)
-		inputGifBossTmp.LoopCount = -1
-		inputGifBossTmp.Config = image.Config{ColorModel: inputGifBossTmp.Config.ColorModel, Width: 500, Height: 500}
-		finalGif, _ := os.OpenFile(path.Join(imagesRoot, imgNmeFiles[i]), os.O_CREATE|os.O_WRONLY|os.O_TRUNC, os.ModePerm)
-
-		err = gif.EncodeAll(finalGif, inputGifBossTmp)
-		if err != nil {
-			println(err.Error())
-			debug.PrintStack()
-			return
-		}
-		err = finalGif.Close()
-		if err != nil {
-			println(err.Error())
-			return
-		}
-	}
 }
